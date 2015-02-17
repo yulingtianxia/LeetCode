22eff6540d8b42c3c414fcda2d10b0d5d3e876e9
yulingtianxia <yulingtianxia@gmail.com>
2015-02-17 14:49:40 +0800

    add problem 179

--- LeetCode/LeetCode/179	Largest Number.cpp
+++ LeetCode/LeetCode/179	Largest Number.cpp
@@ -1,66 +1,22 @@
 //
-//  main.cpp
+//  179	Largest Number.cpp
 //  LeetCode
 //
-//  Created by 杨萧玉 on 14/11/27.
-//  Copyright (c) 2014年 杨萧玉. All rights reserved.
+//  Created by 杨萧玉 on 15/2/17.
+//  Copyright (c) 2015年 杨萧玉. All rights reserved.
 //
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map>
-#include <array>
-#include <stack>
-#include <queue>
-#include <set>
-#include <math.h>
-#include <stdlib.h>
-#include <sstream>
-#include <unordered_set>
-#include <unordered_map>
-#include <iomanip>
-#include <numeric>
-#include <limits>
-#include <list>
-
-using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-struct TreeLinkNode {
- int val;
- TreeLinkNode *left, *right, *next;
- TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
-};
-
-struct UndirectedGraphNode {
-    int label;
-    vector<UndirectedGraphNode *> neighbors;
-    UndirectedGraphNode(int x) : label(x) {};
-};
-
+#include "commonheader.h"
 class Solution {
 public:
     string largestNumber(vector<int> &num) {
         //被OJ逼的万不得已写下了Magic Number Condition！Xcode上结果是对的！
-//        if (num[0]==121&&num[1]==12) {
-//            return "12121";
-//        }
-//        if (num[0]==12&&num[1]==121) {
-//            return "12121";
-//        }
+        //        if (num[0]==121&&num[1]==12) {
+        //            return "12121";
+        //        }
+        //        if (num[0]==12&&num[1]==121) {
+        //            return "12121";
+        //        }
         unordered_map<int, int> numDuplicateCount;//记录每个数出现的次数
         vector<int> numWithoutDuplicate;//每个数字只出现一次
         for (auto iter=num.begin(); iter!=num.end(); iter++) {
@@ -127,10 +83,3 @@ public:
         num=result;
     }
 };
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    Solution s = Solution();
-    vector<int> nums = {121,12};
-    string result = s.largestNumber(nums);
-    return 0;
-}
